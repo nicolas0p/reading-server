bb8b1dc1cb086e780e5f2aeae3bf16ed639bd5ba
nicolas0p <nicolas0pfeifer@gmail.com>
2020-08-14 09:34:12 -0300

    Refactor code handling ones and tens
    Created handle_ones and handle_tens_and_ones using code from
    portuguese_number_reader.
    Also modified the way irregular numbers are treated so that it is more general.

--- number_reader.py
+++ number_reader.py
@@ -1,10 +1,41 @@
 
+## Writes a number containing one digit in full
+# @param number number to be written in full
+# @param digit_reading list of readings of single digits
+# @return string containing the reading in full of the number
+def handle_ones(number, digit_reading):
+    if number == 0:
+        return ""
+    return digit_reading[number - 1]
+
+## Writes a number containing two digits in full
+# @param number number to be written in full
+# @param digit_reading list of readings of single digits
+# @param tens_reading list of readings for multiples of ten
+# @param irregulars_reading dict of {irregular_number : its reading}
+# @param filler word that goes between number readings
+# @return string containing the reading in full of the number
+def handle_tens_and_ones(number, digit_reading, tens_reading, irregulars_reading, filler):
+    if number in irregulars_reading:
+        return irregulars_reading[number]
+    ones_digit = int(str(number)[-1])
+    ones = handle_ones(ones_digit, digit_reading)
+    tens_ones = ones
+    if len(str(number)) >= 2 and int(str(number)[-2]) != 0:
+        tens_digit = int(str(number)[-2])
+        tens = tens_reading[tens_digit - 1]
+        if ones_digit == 0:
+            tens_ones = tens
+        else:
+            tens_ones = tens + filler + ones
+    return tens_ones
+
+
 ## Writes a number in full in Portuguese.
 # @param number in [-99999, 99999] integer to be read.
 # @return string containing the given number written in full in Portuguese.
 def portuguese_number_reader(number):
     digit_reading = [
-            "zero",
             "um",
             "dois",
             "três",
@@ -26,17 +57,17 @@ def portuguese_number_reader(number):
             "oitenta",
             "noventa",
             ]
-    irregular_tens_reading = [
-            "onze",
-            "doze",
-            "treze",
-            "quatorze",
-            "quinze",
-            "dezesseis",
-            "dezessete",
-            "dezoito",
-            "dezenove",
-            ]
+    irregulars_reading = {
+            11: "onze",
+            12: "doze",
+            13: "treze",
+            14: "quatorze",
+            15: "quinze",
+            16: "dezesseis",
+            17: "dezessete",
+            18: "dezoito",
+            19: "dezenove",
+            }
     hundreds_reading = [
             "cento",
             "duzentos",
@@ -49,26 +80,16 @@ def portuguese_number_reader(number):
             "novecentos",
             ]
     filler = ' e '
-    ones_digit = int(str(number)[-1])
-    ones = digit_reading[ones_digit]
-    tens_ones = ""
+    if number == 0:
+        return "zero"
+    tens_ones_number = int(str(number)[-2:])
+    tens_ones = handle_tens_and_ones(tens_ones_number, digit_reading,
+            tens_reading, irregulars_reading, filler)
     hundreds = ""
-    tens_ones = ones
-    if len(str(number)) >= 2 and int(str(number)[-2]) != 0:
-        tens_digit = int(str(number)[-2])
-        tens = tens_reading[tens_digit - 1]
-        if ones_digit == 0:
-            tens_ones = tens
-        else:
-            tens_ones = tens + filler + ones
-            if tens_digit == 1:
-                tens_ones = irregular_tens_reading[ones_digit - 1]
-
     if len(str(number)) >= 3:
         hundreds_digit = int(str(number)[-3])
         hundreds = hundreds_reading[hundreds_digit - 1]
-        if tens_ones == "zero":
-            tens_ones = ""
+        if tens_ones == "":
             if hundreds_digit == 1:
                 hundreds = "cem"
         else:
