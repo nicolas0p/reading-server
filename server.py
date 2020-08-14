8db347b25efe46a86ada540d648e1f4f7c3e1168
nicolas0p <nicolas0pfeifer@gmail.com>
2020-08-14 13:16:47 -0300

    Fix do_GET to handle the request accordingly
    For a invalid request (parameter is not an integer or is greater than 99999 or
    smaller than -99999), reply a 400 Bad Request.
    For a valid request, reply a 200 OK with a json dict with the correct key
    containing the number written in full.

--- server.py
+++ server.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 from sys import argv
+
+from number_reader import Portuguese_number_reader as number_reader
 
 class Reading_server(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        request_line = self.requestline
-        print("Got a GET request with request line={}".format(request_line))
+        print("Got a GET request with path = {}".format(self.path))
+        try:
+            number = int(self.path[1:])
+            written_number = number_reader.read_number(number)
+        except ValueError:
+            self.send_response(400)
+            self.end_headers()
+            return
         self.send_response(200)
+        self.send_header("Content-type", "MIME type: application/json")
         self.end_headers()
-        #codes:
-            #successful: 200
-            #client error: 400
+        json_key = number_reader.json_key
+        response = {json_key: written_number}
+        self.wfile.write(json.dumps(response).encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=Reading_server, port=8080):
+def run(port=8080):
     server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
+    httpd = HTTPServer(server_address, Reading_server)
     try:
         print("Reading server: starting server")
         print("Reading server: listening on port {}".format(str(port)))
+        print("Reading server: interrupt process to close server")
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
